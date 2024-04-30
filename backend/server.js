1234576e4f2763541cb8dccfaa9e08ea52231715
Junyi Zeng <junyz17@uci.edu>
2024-04-30 14:17:26 -0700

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,15 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const cors = require('cors');
 app.use(express.json());
-app.use(cors(
-  {
-    origin: 'http://127.0.0.1:5500',
-    origin: 'http://localhost:5500',
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const userRoute = require("./routes/User.js");
 app.use("/user", userRoute);
