e4658e4566161f1e46a67c02f16f68e0fb01d7e7
Jun-Vic <junz26@uci.edu>
2024-05-03 05:16:07 -0700

    adjust navbar and footer
    make sure all links and buttons work as expect

---
+++ frontend/global_styles/loadCompents.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const pathPrefix = determinePath();
+    // Adjust the path to point to the new location of the HTML components
+    loadComponent(pathPrefix + "frontend/global_styles/navbar.html", "navbar");
+    loadComponent(pathPrefix + "frontend/global_styles/footer.html", "footer");
+});
+
+function determinePath() {
+    const location = window.location.pathname;
+    const directoriesNeedingAdjustment = ['/feedback/', '/about/', '/home/', '/login/', '/profile/', '/profile_change/', '/register/', '/chatroom/'];
+
+    // Check if the current location includes any of the specified directories
+    if (directoriesNeedingAdjustment.some(dir => location.includes(dir))) {
+        return '../../'; // Adjusting because the depth has increased
+    } else {
+        return './'; // No change needed for root or same level directory access
+    }
+}
+
+function loadComponent(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById(elementId).innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Failed to load the component:', error);
+        });
+}
